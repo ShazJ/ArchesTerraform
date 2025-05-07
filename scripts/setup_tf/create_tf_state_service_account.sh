12012e956e440861c0f08ea21afb5a993367dc81
sharon.ingram <sharonjingram@hotmail.co.uk>
2025-05-07 20:22:00 +0100

    t

--- scripts/setup_tf/create_tf_state_service_account.sh
+++ scripts/setup_tf/create_tf_state_service_account.sh
@@ -60,17 +60,17 @@ gcloud iam service-accounts create "${TF_SA_NAME}" \
   --display-name="${TF_SA_DISPLAY_NAME}" \
   --project="${PROJECT_ID}" \
   --description="Service account for Terraform"
-
+echo "here"
 # Delay to ensure the service account is created before assigning roles
 #without this the assign roles command fails
 sleep 5
-
+echo "slept"
 # Check if the service account was created successfully
 if ! gcloud iam service-accounts list --project="${PROJECT_ID}" | grep -q "${TF_SA_NAME}"; then
   echo "Error: Service account '${TF_SA_NAME}' was not created successfully."
   exit 1
 fi
-
+echo "roles"
 # Assign roles to the service account
 for ROLE in "${ROLES[@]}"; do
   echo "Assigning role ${ROLE} to service account..."
