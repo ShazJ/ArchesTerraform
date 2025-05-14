fcf71163e5f64583ee9255f8489b4b064aff1a44
sharon.ingram <sharonjingram@hotmail.co.uk>
2025-05-14 11:05:36 +0100

    t

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.35"
+      version = "~> 5.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
