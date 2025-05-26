3524e3b5fa846785dae9b5f87dd423ebb1e7f7fe
sharon.ingram <sharonjingram@hotmail.co.uk>
2025-05-26 16:42:54 +0100

    fmt

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ module "compute_router" {
 
 module "kms_key_ring" {
   for_each         = var.kms_key_rings
-  depends_on = [ module.service_accounts ]
+  depends_on       = [module.service_accounts]
   source           = "./modules/kms"
   project_id       = var.project_id
   name             = each.value.name
@@ -170,8 +170,8 @@ module "kms_key_ring" {
 
 # Root module to manage GKE clusters and node pools for multiple environments
 module "container_cluster" {
-  source = "./modules/container_cluster"
-  depends_on = [ module.compute_subnetwork, module.compute_subnetwork_prd, module.compute_network, module.compute_network_prd, module.compute_router ]
+  source     = "./modules/container_cluster"
+  depends_on = [module.compute_subnetwork, module.compute_subnetwork_prd, module.compute_network, module.compute_network_prd, module.compute_router]
 
   for_each = var.clusters
 
